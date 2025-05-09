07a7b37ebee99867dad343635f9f6e913fd4ef6b
Ali <49276816+Furqanalimir@users.noreply.github.com>
2025-05-09 09:13:26 +0000

    cleaned up code and added scheduler

--- utils/cron.go
+++ utils/cron.go
@@ -5,29 +5,31 @@ import (
 	"time"
 
 	"github.com/robfig/cron/v3"
+	"currency-service/client"
 )
 
 var CronScheduler *cron.Cron
 
-// Initialize the cron scheduler
 func InitCron() {
 	CronScheduler = cron.New()
 
-	// Example: Run a job every minute
-	_, err := CronScheduler.AddFunc("* * * * *", exampleJob)
+	_, err := CronScheduler.AddFunc("*/30 * * * *", exchangeRateJob)
 	if err != nil {
-		fmt.Println("Failed to schedule example job:", err)
+		fmt.Println("Failed to schedule job:", err)
 	}
 
 	CronScheduler.Start()
 }
 
-// Example job function
-func exampleJob() {
-	fmt.Println("Running example job at", time.Now())
+func exchangeRateJob() {
+	date := time.Now()
+	rate, err := client.GetExchangeRate("USD", "INR", date)
+	if err != nil {
+		return 0, err
+	}
+	return amount * rate, nil
 }
 
-// Add a custom job with a schedule
 func AddCronJob(spec string, jobFunc func()) (cron.EntryID, error) {
 	if CronScheduler == nil {
 		InitCron()
